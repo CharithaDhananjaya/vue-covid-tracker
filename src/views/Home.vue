320c1fac03af55122647aa4897bb3463695d2544
Yasurindi Manavi <yasurindi4@gmail.com>
2021-05-13 13:45:37 +0530

    Initial Change.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to Vue COVID Tracker Developed by Yasurindi Manavi"/>
   </div>
 </template>
 
