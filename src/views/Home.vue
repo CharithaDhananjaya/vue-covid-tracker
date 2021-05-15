5960c0b3c02d0f0dbfd14642185370c7d023b32e
Charitha Dhananjaya <charitha.dhananjaya464@gmail.com>
2021-05-15 23:38:23 +0530

    country select add

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,7 @@
   <main v-if="!loading">
     <DataTitle :text="title" :dataDate="dataDate" />
     <DataBoxes :stats="stats" />
+    <CountrySelect @get-country="getCountryData" :countries="countries" />
   </main>
   <main v-else class="flex flex-col align-center justify-center text-center">
     <div class="text-gray-500 text-3xl mt-10 mb-6">Fetching Data</div>
@@ -12,12 +13,14 @@
 <script>
 import DataTitle from "../components/DataTitle";
 import DataBoxes from "../components/DataBoxes";
+import CountrySelect from "../components/CountrySelect";
 
 export default {
   name: "Home",
   components: {
     DataTitle,
     DataBoxes,
+    CountrySelect,
   },
   data() {
     return {
@@ -34,6 +37,10 @@ export default {
       const res = await fetch("https://api.covid19api.com/summary");
       const data = await res.json();
       return data;
+    },
+    getCountryData(country) {
+      this.stats = country;
+      this.title = country.Country;
     },
   },
   async created() {
