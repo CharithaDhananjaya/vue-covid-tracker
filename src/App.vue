adc714b80a7d79e6832e024af62b859ebcd5a52b
Charitha Dhananjaya <charitha.dhananjaya464@gmail.com>
2021-05-16 00:01:18 +0530

    master branch fixed.

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,6 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+  <h2>COVID19 Tracker</h2>
+  <router-view />
 </template>
 
 <style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,9 +1,8 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
     <p>
-      This is the master branchbuild, please checkout from branches either
-      Charitha or Yasurindi
+      This is the master branch build, please checkout from branches either
+      Charitha or Yasurindi in your repository.
     </p>
   </div>
 </template>
@@ -11,26 +10,5 @@
 <script>
 export default {
   name: "HelloWorld",
-  props: {
-    msg: String,
-  },
 };
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
