0aae4c21387c828476364bb4430a305d9cfd255a
Yasurindi Manavi <yasurindi4@gmail.com>
2021-05-15 19:13:52 +0530

    Final step

--- src/components/DataBoxes.vue
+++ src/components/DataBoxes.vue
@@ -1,6 +1,6 @@
 <template>
 <!-- Box 1 -->
-  <div class="grid md:grid-colos-2 gap-4">
+  <div class="grid md:grid-cols-2 gap-4">
       <div class="shadow-md bg-blue-100 p-10 text-center rounded">
           <h3 class="text-3xl text-blue-900 font-bold mb-4">Cases</h3>
           <div class="text-2xl mb-4">
@@ -12,9 +12,7 @@
               {{ numberWithCommas(stats.TotalConfirmed) }} 
           </div>
       </div>
-  </div>
   <!-- Box 2 -->
-  <div class="grid md:grid-colos-2 gap-4">
       <div class="shadow-md bg-blue-200 p-10 text-center rounded">
           <h3 class="text-3xl text-blue-900 font-bold mb-4">Deaths</h3>
           <div class="text-2xl mb-4">
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,13 @@
 <template>
   <main v-if="!loading">
-    Show Data
+    <DataTitle :text="title" :dataDate="dataDate" />
+
+    <DataBoxes :stats="stats" />
+
+    <CountrySelect @get-country="getCountryData" :countries="countries" />
+
+    <button @click="clearCountryData"
+    v-if="stats.Country"  class="bg-green-700 text-white rounded p-3 mt-10 focus:outline-none hover:bg-green-600">Clear Country</button>
   </main>
 
   <main class="flex flex-col align-center justify-center text-center " v-else>
@@ -12,11 +19,18 @@
 </template>
 
 <script>
+import DataTitle from '@/components/DataTitle'
+import DataBoxes from '@/components/DataBoxes'
+import CountrySelect from '@/components/CountrySelect'
 
 
 export default {
   name: 'Home',
-  components: {},
+  components: {
+    DataTitle,
+    DataBoxes,
+    CountrySelect
+  },
   data () {
     return {
       loading: true,
@@ -32,16 +46,26 @@ export default {
       const res = await fetch('https://api.covid19api.com/summary')
       const data = await res.json()
       return data
+    },
+    getCountryData(country) {
+      this.stats = country
+      this.title = country.Country
+    },
+    async clearCountryData() {
+      this.loading = true
+      const data = await this.fetchCovidData()
+      this.title = 'Global'
+      this.stats = data.Global
+      this.loading = false
     }
   },
   async created() {
     const data = await this.fetchCovidData()
     
-
     this.dataDate = data.Date
     this.stats = data.Global
     this.countries = data.Countries
-    this.loading = true
+    this.loading = false
   }
 }
 </script>
