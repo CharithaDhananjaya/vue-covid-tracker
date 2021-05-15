dda519475c68da3a2f16b4c4e0c84916a3efd758
Charitha Dhananjaya <charitha.dhananjaya464@gmail.com>
2021-05-15 22:45:07 +0530

    data loding and titel component set.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,45 @@
 <template>
-  <div class="home">Hello World</div>
+  <main v-if="!loading">
+    <DataTitle :text="title" :dataDate="dataDate" />
+  </main>
+  <main v-else class="flex flex-col align-center justify-center text-center">
+    <div class="text-gray-500 text-3xl mt-10 mb-6">Fetching Data</div>
+    <img :src="loadingImage" alt="" class="w-40 m-auto" />
+  </main>
 </template>
 
 <script>
+import DataTitle from "../components/DataTitle";
 export default {
   name: "Home",
-  components: {},
+  components: {
+    DataTitle,
+  },
+  data() {
+    return {
+      loading: true,
+      title: "Global",
+      dataDate: "",
+      stats: "",
+      countries: [],
+      loadingImage: require("../assets/loading.gif"),
+    };
+  },
+  methods: {
+    async fetchCovidData() {
+      const res = await fetch("https://api.covid19api.com/summary");
+      const data = await res.json();
+      return data;
+    },
+  },
+  async created() {
+    const data = await this.fetchCovidData();
+    console.log(data);
+
+    this.dataDate = data.Date;
+    this.stats = data.Global;
+    this.countries = data.Countries;
+    this.loading = false;
+  },
 };
 </script>
